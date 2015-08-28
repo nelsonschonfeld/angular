5e61d4ebcdf29e4efcab8badce045d88baeabbec
nschonfeld <nschonfeld@pc005014.mparana.gov.ar>
2015-08-28 10:59:21 -0300

    instalando angular y saludo

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html ng-app>
+    <head>
+        <meta charset="UTF-8">
+        <title>Prueba de Angular</title>
+        <script src="includes/angular/angular.min.js" ></script>
+    </head>
+    <body>
+        <h1>Curso Angular</h1>
+        <p>{{ "Hola Angular"}}</p>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
